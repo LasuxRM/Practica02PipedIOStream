cdd857c654790a6cbaffe9eab6a0ca79869a00e4
LasuxRM <lucyall19@gmail.com>
2020-09-12 16:37:28 -0500

    Finalizada primera mitad

--- src/piped_io_Stream/main.java
+++ src/piped_io_Stream/main.java
@@ -2,16 +2,26 @@ package piped_io_Stream;
 
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) throws IOException{
 		
-		Line	linea01		= new Line(0,5);
-		Line	lineafinal	= new Line(0,0);
-		int[]	ConjNumbers	= new int[] {5,1,4,2,3};
-		int[]	ConjNumReception	= new int[ConjNumbers.length];
+		Scanner sc = new Scanner(System.in);
+		
+//		Line	linea01		= new Line(0,5);
+//		Line	lineafinal	= new Line(0,0);
+		int[]	NumberSet	= new int[5];
+		int[]	NumberSetSorted	= new int[NumberSet.length];
 				
+		
+		System.out.println("Enter five numbers");
+		for (int i=0; i<5; i++) {
+			NumberSet[i] = sc.nextInt();
+		}
+		//int a=System.in.read();
+		
 		PipedInputStream	pins	= new PipedInputStream(); 
 	    PipedOutputStream	pouts	= new PipedOutputStream();
 	    
@@ -21,13 +31,11 @@ public class main {
 	    	public void run() {
 	    		
 	    		try {
-	    			for(int i=0; i<ConjNumbers.length; i++) {
-	    				
-	    				//pouts.write(linea01.getEjeX());
-	    				
-	    				pouts.write(ConjNumbers[i]);
-	    				System.out.println("PipedOutputStream escribiendo ConjNumbers["+i+"]="+ConjNumbers[i]);
-	    				Thread.sleep(2000);
+	    			for(int i=0; i<NumberSet.length; i++) {
+	    					    				
+	    				pouts.write(NumberSet[i]);
+	    				System.out.println("PipedOutputStream writing NumberSet["+i+"]="+NumberSet[i]);
+	    				Thread.sleep(1000);
 	    			}
 	    			pouts.close();
 	    			
@@ -44,8 +52,8 @@ public class main {
 	    		try {
 	    			int n=0,j;
 	    			while((j = pins.read()) != -1) {
-	    				System.out.println("PipedInputStream Leyendo j="+j);
-	    				ConjNumReception[n]=j;
+	    				System.out.println("PipedInputStream reading j="+j);
+	    				NumberSetSorted[n]=j;
 	    				n++;
 	    			}
 	    			pins.close();
@@ -54,15 +62,19 @@ public class main {
 	    			System.out.println(e);
 	    		}
 	    		
-	    		Arrays.sort(ConjNumReception);
-	    		for(int k=0; k<ConjNumReception.length; k++) {
-	    			
-	    	    	System.out.print(ConjNumReception[k]);
+	    		//Sorting
+	    		Arrays.sort(NumberSetSorted);
+	    		
+	    		//Showing sorted set
+	    		for(int k=0; k<NumberSetSorted.length; k++) {
+	    	    	System.out.print(NumberSetSorted[k]);
 	    	    }
+	    		
+	    		//Showing inverted sorted set
 	    		System.out.print("\n");
-	    		for(int k=ConjNumReception.length-1; k>-1; k--) {
+	    		for(int k=NumberSetSorted.length-1; k>-1; k--) {
 	    			
-	    	    	System.out.print(ConjNumReception[k]);
+	    	    	System.out.print(NumberSetSorted[k]);
 	    	    }
 	    	}
 	    };
